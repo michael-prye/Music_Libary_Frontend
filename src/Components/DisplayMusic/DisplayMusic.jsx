ab1ed4013788270ff2c96f2a8b99a9aefb6929e7
michael-prye <michael.prye@gmail.com>
2022-04-27 12:01:12 -0400

    Created table map and display songs from the database + testing Carousel from bootstrap

--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -1,18 +1,47 @@
-import Song from "../Song/Song"
-const DisplayMusic = (props) => {
-    return ( 
-        <table>
-            <tbody>
-                {props.songs.map((song)=>{
-                    return(
-                        <tr><Song title = {song.title} artist={song.artist} album = {song.album} album_art={song.album_art} release_date = {song.release_date} genre={song.genre} likes={song.likes} /></tr>
-                    )
-                }
+import Song from "../Song/Song";
+import React from "react";
+import { Carousel } from "bootstrap";
 
-                )}
-            </tbody>
-        </table>
-     );
+
+
+
+
+const DisplayMusic = (props) => {
+    const song = props.song;
+    return (
+        <Carousel>
+            {song.map((song)=>{
+                return(
+                    <Carousel.Item>
+                        <Song title = {song.title} artist={song.artist} album = {song.album} album_art={song.album_art} release_date = {song.release_date} genre={song.genre} likes={song.likes} />                        
+                    </Carousel.Item>
+                )
+            }
+            )}
+        </Carousel>
+
+        // <table>
+        //     <tbody>
+        //         {props.songs.map((song)=>{
+        //             return(
+        //                 <tr><Song title = {song.title} artist={song.artist} album = {song.album} album_art={song.album_art} release_date = {song.release_date} genre={song.genre} likes={song.likes} /></tr>
+        //             )
+        //         }
+
+        //         )}
+        //     </tbody>
+        // </table>
+
+
+    );
 }
  
 export default DisplayMusic;
+
+// {props.songs.map((song)=>{
+//     return(
+//         <tr><Song title = {song.title} artist={song.artist} album = {song.album} album_art={song.album_art} release_date = {song.release_date} genre={song.genre} likes={song.likes} /></tr>
+//     )
+// }
+
+// )}
